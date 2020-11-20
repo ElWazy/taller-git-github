ef3b6b969d5941f3b958f8750f79cc3b98e74d73
estebanvivanco <esteban.vivanco.progra@gmail.com>
2020-11-20 14:22:39 -0300

    Agregue mi nombre jijoioik

--- src/org/elwazy/proyecto/Main.java
+++ src/org/elwazy/proyecto/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         String[] asistencia = new String[] {
-                "Santiago"
+                "Santiago",
+                "Esteban V"
         };
 
         for (String almuno : asistencia) {
