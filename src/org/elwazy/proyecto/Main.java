cf8cd1efbb312387607c2962663f0902eb7e72a5
ElWazy <santiago.fierro4@outlook.cl>
2020-11-20 13:34:47 -0300

    nos despedimos del mundo

--- src/org/elwazy/proyecto/Main.java
+++ src/org/elwazy/proyecto/Main.java
@@ -3,6 +3,6 @@ package org.elwazy.proyecto;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Chao mundo");
     }
 }
